f381b133fb39edf4cff0bc9df552dc4f649762e6
IcyMods <163366867+IcyMods@users.noreply.github.com>
2025-01-07 17:53:07 -0500

    Update squidgame.lua

--- squidgame.lua
+++ squidgame.lua
@@ -144,12 +144,12 @@ local function autoFarmV2()
             print("Moving to finish position...")
             -- Move the character to the finish position (Position, not CFrame)
             character.HumanoidRootPart.Position = finish.Position
-            task.wait(1)  -- Wait for the character to reach the chest
+            task.wait(3)  -- Wait for the character to reach the chest
 
             currentState = true
         elseif currentState == true then
             print("Waiting before switching state back to false.")
-            task.wait(0.55)  -- Wait before switching back to false
+            task.wait(3)  -- Wait before switching back to false
             currentState = false
         end
 
