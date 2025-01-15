576e62617e6c69fb79a89cff18315e1291e52591
IcyMods <163366867+IcyMods@users.noreply.github.com>
2025-01-14 21:23:20 -0500

    Update shrimpGame.lua

--- shrimpGame.lua
+++ shrimpGame.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
     Name = "[🪖GUARD] Shrimp Game",
     Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-    LoadingTitle = "[🪖GUARD] Shrimp Game | v1.0.5",
+    LoadingTitle = "[🪖GUARD] Shrimp Game | v1.0.7",
     LoadingSubtitle = "by @avvexxy",
     Theme = "Dark Blue ", -- Check https://docs.sirius.menu/rayfield/configuration/themes
  
