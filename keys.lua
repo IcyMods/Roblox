f596fbe17648b23cb3cf8873ae0cf70ee0ab925c
IcyMods <163366867+IcyMods@users.noreply.github.com>
2025-01-25 22:27:39 -0500

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,23 +1,4 @@
 return {
-    { key = "UT3EO-840R6-I73JD-8XST6", expires = 1737843720 },
-    { key = "IWYYP-WIUAW-ZST7A-HO6K5", expires = 1737843840 }, 
-    { key = "FLE2Y-A31KH-SUZ1G-OO3MK", expires = 1740279600 }, 
-    { key = "6IF45-S6OTW-CY1FJ-SB2BD", expires = 1737929400 }, 
-    { key = "G4MFD-QE025-WHHLG-QQ6TQ", expires = 1737929400 }, 
-    { key = "CH0XX-1XXTH-ZGUSM-7BSBY", expires = 1737929400 }, 
-    { key = "3XYB6-21XF2-8JZ2M-J9G3G", expires = 1737929400 }, 
-    { key = "X23PO-FWOXB-WLDFJ-E6H81", expires = 1737929400 }, 
-    { key = "8MJVV-L8NKO-X6X7X-A05SK", expires = 1737929400 }, 
-    { key = "REGDO-8GO45-4LP93-RR0DT", expires = 1737929400 }, 
-    { key = "2WWJ0-E1SK5-JJ85F-D169W", expires = 1737929400 }, 
-    { key = "RLDHR-2JBN8-34TLO-FK648", expires = 1737929400 }, 
-    { key = "SV1W3-EQHPR-BVQAS-0Q983", expires = 1737929400 }, 
-    { key = "41TIR-265A0-9V2C5-9A3WH", expires = 1737929400 }, 
-    { key = "CPTYZ-Y3DPY-YAUKZ-LL5RV", expires = 1737929400 },
-    { key = "FOY6P-FWFXE-8H4PX-QOHDR", expires = 1737929400 }, 
-    { key = "CKX0W-BR9FO-8DV2T-TADFG", expires = 1737929400 }, 
-    { key = "CGW84-JSYIA-I6VIF-86DFY", expires = 1737929400 }, 
-    { key = "3M6L6-I1ILI-PY1WU-GSQ52", expires = 1737929400 }, 
-    { key = "QQQ0T-4BLV0-0SMN2-SVFFZ", expires = 1737929400 }, 
-    { key = "O7UIP-ET25M-M8WZU-9IQLB", expires = 1737929400 }
+    { key = "UT3EO-840R6-I73JD-8XST6", expires = 1739052000 }, -- 2025, FEB 8TH
+    { key = "IWYYP-WIUAW-ZST7A-HO6K5", expires = 1740261600 },  -- 2025, FEB 22TH
 }
