b1d133ad46a12c497c15219dfab45d7d3324f55c
IcyMods <163366867+IcyMods@users.noreply.github.com>
2025-01-25 21:33:41 -0500

    Create keys.lua

---
+++ keys.lua
@@ -0,0 +1,4 @@
+return {
+    { key = "UT3EO-840R6-I73JD-8XST6", expires = 1737927600 }, -- expires in 9 minutes
+    { key = "NEW-KEY-123456-ABCDEF", expires = 1737929400 } -- expires in 30 minutes
+}
