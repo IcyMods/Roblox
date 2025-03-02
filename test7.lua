b35cee31cd4f44f4ef63d31d1267a57f4423bf3f
IcyMods <163366867+IcyMods@users.noreply.github.com>
2025-03-02 02:16:26 -0500

    Update test7.lua

--- test7.lua
+++ test7.lua
@@ -1,5 +1,6 @@
 -- WORKING ON A ADVANCDED OBFUSCATOR FOR MY SCRIPTS!
 
 
---Obfuscated Code: -- this prints hello world but yeah
-LuaT.�xV...........(w@�return "����ӏ�����ȋ�����Æ���"��.�Q......F.F.������ӏ�����ȋ�����Æ����..��...����_ENV
+--Obfuscated Code:
+ SEpRVkwQGnBdVFRXFBhPV0pUXBkaETIA
+
